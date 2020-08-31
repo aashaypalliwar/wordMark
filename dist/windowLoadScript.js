8d6c9b17b71aa1efb608ddbb0a873866c558bc0d
p_ash <palliwar.aashay@gmail.com>
2020-08-31 19:00:43 +0530

    [Chore] Cleaned up files

--- dist/windowLoadScript.js
+++
@@ -1,9 +0,0 @@
-var windowHasLoaded = false;
-
-var navData = window.performance.getEntriesByType("navigation");
-if (navData.length > 0 && navData[0].loadEventEnd > 0)
-{
-    windowHasLoaded = true;
-} else {
-    console.log('Document is not loaded');
-}
